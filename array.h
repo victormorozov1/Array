c8e547f56a8a463d8b49ca02ce88a1c4abcf0251
victormorozov1 <vismo@bk.ru>
2022-04-17 12:49:00 +0500

    add class Array

---
+++ array.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdexcept>
+
+class ArrayOutOfRange : public std::out_of_range {
+ public:
+  ArrayOutOfRange() : std::out_of_range("ArrayOutOfRange") {
+  }
+};
+
+template <typename type, int sz>
+class Array {
+ public:
+  type array[sz];
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "array.h"
+
+using namespace std;
+
+int main() {
+  Array<int, 5>a{3,4,6,7,9};
+  cout << a.array[4];
+}
